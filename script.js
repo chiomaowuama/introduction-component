2971373684f991ee51c01207fd1de615db9d4e7c
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-06-16 00:54:02 +0100

    still on script

--- script.js
+++ script.js
@@ -10,17 +10,21 @@ function submits(event) {
   let Sname = document.getElementById("secondname").value;
   let errorsec = document.getElementById("errorsec");
   let Emailad = document.getElementById("emails").value;
+  let Emailerror = document.getElementById("Emailerror");
   let Passwords = document.getElementById("password").value;
-  console.log(Fname + ", " + Sname + "," + Emailad + ", " + Passwords);
+  let PasswordsError = document.getElementById("passworderror");
   let result = [];
+  let isValid = true;
 
   const regex = /^[A-Za-z]+$/;
   //   firstname verification
   if (Fname === "" || Fname == null) {
     errorfirst.innerHTML = "please input firstname";
+    isValid = false;
   } else {
     if (!regex.test(Fname)) {
       errorfirst.innerHTML = "please Alphabets only and no space";
+      isValid = false;
     } else {
       errorfirst.innerHTML = "";
       result.firstname = Fname;
@@ -29,12 +33,41 @@ function submits(event) {
   // surnname verification
   if (Sname === "" || Sname == null) {
     errorsec.innerHTML = "please input Surnname";
+    isValid = false;
   } else {
     if (!regex.test(Sname)) {
       errorsec.innerHTML = "please Alphabets only";
+      isValid = false;
     } else {
       errorsec.innerHTML = "";
       result.Surnname = Sname;
     }
   }
+  //   Email verification
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (Emailad === " " || Emailad == null) {
+    Emailerror.innerHTML = "please input Email";
+    isValid = false;
+  } else {
+    if (!emailRegex.test(Emailad)) {
+      Emailerror.innerHTML = "Email incorrect";
+      isValid = false;
+    } else {
+      Emailerror.innerHTML = "";
+      result.email = Emailad;
+    }
+  }
+  //   password
+  if (Passwords === "" || Passwords == null) {
+    PasswordsError.innerHTML = "please input password";
+    isValid = false;
+  } else {
+    PasswordsError.innerHTML = "";
+    result.password = Passwords;
+  }
+
+  if (!isValid) {
+    return;
+  }
+  console.log("Form submitted with data:", result);
 }
